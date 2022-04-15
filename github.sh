4dd4743481773f25dc36c1b8aebcd366e12a7f2e
unknown <joekh16@gmail.com>
2022-04-15 15:55:49 -0700

    changed file

--- github.sh
+++ github.sh
@@ -1,4 +1,4 @@
 source github_credentials.sh
-git add main.yml deploy-infra.sh server.js
-git commit -m "Add a second instance"
+git add main.yml deploy-infra.sh
+git commit -m "Add ALB Load Balancer"
 git push
