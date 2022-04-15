aec753539d9b8d6773314f4fafa7a8c2f60493f2
unknown <joekh16@gmail.com>
2022-04-15 15:31:07 -0700

    updated files

--- deploy-infra.sh
+++ deploy-infra.sh
@@ -3,26 +3,20 @@ source aws_credentials.sh
 STACK_NAME=awsbootstrap
 REGION=us-east-1 
 CLI_PROFILE=awsbootstrap
-
 EC2_INSTANCE_TYPE=t2.micro 
 
-AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap \
-  --query "Account" --output text`
+GH_ACCESS_TOKEN=$(cat ~/.github/aws-bootstrap-access-token)
+GH_OWNER=$(cat ~/.github/aws-bootstrap-owner)
+GH_REPO=$(cat ~/.github/aws-bootstrap-repo)
+GH_BRANCH=master
+
+AWS_ACCOUNT_ID=`aws sts get-caller-identity --profile awsbootstrap --query "Account" --output text`
 CODEPIPELINE_BUCKET="$STACK_NAME-$REGION-codepipeline-$AWS_ACCOUNT_ID" 
 
-# Deploy the CloudFormation template
-echo -e "\n\n=========== Deploying main.yml ==========="
-aws cloudformation deploy \
-  --region $REGION \
-  --profile $CLI_PROFILE \
-  --stack-name $STACK_NAME \
-  --template-file main.yml \
-  --no-fail-on-empty-changeset \
-  --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE
+Echo $CODEPIPELINE_BUCKET
 
 # Deploys static resources
-echo -e "\n\n=========== Deploying setup.yml ==========="
+echo "\n\n=========== Deploying setup.yml ==========="
 aws cloudformation deploy \
   --region $REGION \
   --profile $CLI_PROFILE \
@@ -30,7 +24,22 @@ aws cloudformation deploy \
   --template-file setup.yml \
   --no-fail-on-empty-changeset \
   --capabilities CAPABILITY_NAMED_IAM \
-  --parameter-overrides \
+  --parameter-overrides CodePipelineBucket=$CODEPIPELINE_BUCKET
+
+# Deploy the CloudFormation template
+echo "\n\n=========== Deploying main.yml ==========="
+aws cloudformation deploy \
+  --region $REGION \
+  --profile $CLI_PROFILE \
+  --stack-name $STACK_NAME \
+  --template-file main.yml \
+  --no-fail-on-empty-changeset \
+  --capabilities CAPABILITY_NAMED_IAM \
+  --parameter-overrides EC2InstanceType=$EC2_INSTANCE_TYPE \
+    GitHubOwner=$GH_OWNER \
+    GitHubRepo=$GH_REPO \
+    GitHubBranch=$GH_BRANCH \
+    GitHubPersonalAccessToken=$GH_ACCESS_TOKEN \
     CodePipelineBucket=$CODEPIPELINE_BUCKET
 
     # If the deploy succeeded, show the DNS name of the created instance
