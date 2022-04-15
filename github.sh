98c0fbbbc344740c28cea8830bd4d329a37b22b9
unknown <joekh16@gmail.com>
2022-04-15 15:46:26 -0700

    updated deploy-infra.sh

--- github.sh
+++ github.sh
@@ -1,4 +1,4 @@
 source github_credentials.sh
-git add appspec.yml buildspec.yml start-service.sh stop-service.sh deploy-infra.sh setup.yml main.yml
-git commit -m "Add CodeDeploy/CodeBuild/CodePipeline"
-git push origin master
+git add main.yml deploy-infra.sh server.js
+git commit -m "Add a second instance"
+git push
